f8b705793f26b8f0ca75659f2c5d3be06f1b040b
Thomas <thomasbourgeois2@hotmail.com>
2024-01-31 14:13:32 -0800

    rewritten entire class

--- steppers.py
+++ steppers.py
@@ -1,184 +1,138 @@
 '''
-
 Written 2024
 
 Micropython module for the DVR8825 Stepper motor drive to used with the Pi Pico.
 
-
+Rotation Directions:
+    - Positive steps rotate counter-clockwise (CCW).
+    - Negative steps rotate clockwise (CW).
 '''
 
 import utime
 from machine import Pin
 
 
-# Constants
-STEP_PER_MM = 80
-MM_PER_STEP = 1 / STEP_PER_MM
+# DO NOT CHANGE
+CCW = 0  # Counter-Clockwise.
+CW = 1  # Clockwise.
 
 
 class Stepper:
-    '''
-    Class to be used with DVR8825 stepper driver.
-
-    Parameters:
-        dir_pin: pin used for dir.
-        step_pin: pin used for step.
-        enabled: pin used for enable.
-
-        # Optional
-        mode_pins:  pins for M0, M1, M2 (default (None, None, None)).
-        step_mode: what step micro-step per phase to use ('FULL', '1/2', '1/4', '1/8', '1/16', '1/32') (default - 'FULL').
-        steps_per_rev: number of steps per revolution in "FULL" step mode, (default 200).
-    '''
-
     def __init__(self,
-                 dir_pin,  # Dir pin
-                 step_pin,  # step pin
-                 enable_pin,  # enable pin
-
-                 # Optional Parameters:
-                 mode_pins=(None, None, None),  # Pins for M0, M1, M2
-                 step_mode='FULL',
-                 steps_per_rev=200,
+                 step_per_rev: int,  # number of steps per revolution
+                 dir_pin: int,  # direction pin #.
+                 step_pin: int,  # step pin #.
+                 enable_pin: int,  # enable pin #.
+                 mode_pins=(None, None, None)  # mode pins (M0, M1, M2)
                  ) -> None:
 
-        self.step_mode = step_mode
-        self.step_per_rev = steps_per_rev
+        self.steps_number = 0  # which step the motor is on.
+        self.direction = CCW  # direction of motor: 0 - CCW, 1 - CW
+        self.steps_per_rev = step_per_rev  # steps per revolution
 
-        # Objects for direction, step and enable pins
+        # direction, step and enable pin objects on Stepper Driver
         self.dir_pin = Pin(dir_pin, Pin.OUT)
         self.step_pin = Pin(step_pin, Pin.OUT)
         self.enable_pin = Pin(enable_pin, Pin.OUT)
 
-        # Scale factors for microsteps per phase for modes
-        self.microsteps = {
-            'FULL': 1,
-            '1/2': 2,
-            '1/4': 4,
-            '1/8': 8,
-            '1/16': 16,
-            '1/32': 32
-        }
-
-        # pin states for mode configuration
-        mode_config = {
-            # M0, M1, M2
-            'FULL': (0, 0, 0),
-            '1/2': (1, 0, 0),
-            '1/4': (0, 1, 0),
-            '1/8': (1, 1, 0),
-            '1/16': (0, 0, 1),
-            '1/32': (1, 0, 1),
-        }
-
-        # if any mode pins have been given intialize them
-        if mode_pins[0]:
-            self.mode0 = Pin(mode_pins[0], Pin.OUT)
-            self.mode0.value(mode_config[step_mode][0])
-        if mode_pins[1]:
-            self.mode1 = Pin(mode_pins[1], Pin.OUT)
-            self.mode1.value(mode_config[step_mode][1])
-        if mode_pins[2]:
-            self.mode2 = Pin(mode_pins[2], Pin.OUT)
-            self.mode2.value(mode_config[step_mode][2])
-
-        self._enable = False  # motor running _enable
+        self.enabled = False  # motor is ready to run
         self.disable()
+
+        # setting default speed
+        self.set_speed(75)
 
     def enable(self) -> None:
         '''
-        Function to enable the motor.
-
-            motor is ready.
+        Function to enable the motor for operation.
         '''
         self.enable_pin.value(0)
-        self._enable = True
-        utime.sleep_ms(10)
+        self.enabled = True
+        utime.sleep_ms(50)
 
     def disable(self) -> None:
         '''
-        Function to enable the motor.
-
-            motor is free.
+        Function to disable the motor from operation.
         '''
+
         self.enable_pin.value(1)
-        self._enable = False
+        self.enabled = False
 
-    def one_step(self, direction: str) -> None:
+    def set_speed(self, speed) -> None:
         '''
-        Function to make motor take one step in a given direction:
+        Function to set the motors speed.
 
         Parameters:
-            direction: 'forward' - CCW, 'backward' - CW
-        '''
-        if direction == 'forward':
-            self.dir_pin.value(0)
-        elif direction == 'backward':
-            self.dir_pin.value(1)
-
-        self.step_pin.value(0)
-        self.step_pin.value(1)
-
-    def move_steps(self, direction: str, step_count: int, delay=0) -> None:
-        '''
-        Function to move a given number of steps in a certain direction.
-
-        Parameters:
-            direction: 'forward' - counter clockwise, 'backward' - clockwise
-            step_count: how many steps to take.
-            delay (default 0): delay time (microseconds) for adjsuting speed.
+            speed: speed of motor in RPM.        
         '''
 
-        if self._enable is False:
-            raise ValueError("Motors must be enable to step.")
+        # Calculating delay time between each step in microseconds (delay/step).
+        # 1. Convert minute to microseconds.
+        # 2. Divide by total number of steps per revolution.
+        # 3. Divide by desired speed in steps per minute.
+        delay = ((60*1e3*1e3) / self.steps_per_rev) / speed
+        self.delay = round(delay)
 
-        if step_count < 0:
-            raise ValueError('Parameter "step_count" must be greater than 0.')
+    def get_speed(self) -> float:
+        '''
+        Function to calculate the speed of the motor in rpm.
+        '''
+        rpm = 1/(self.steps_per_rev * self.delay) * 60 * 1e3 * 1e3
+        return round(rpm, 2)
 
-        if type(delay) is not int:
-            raise ValueError(f'Parameter "delay" must be an integer.')
+    def set_direction(self, direction: int):
+        '''
+        Function to set the direcion of the motor
+        '''
+        if direction not in [CCW, CW]:
+            raise ValueError(
+                'Direction must be either "0 - Counter-Clockwise" or "1 - Clockwise" ')
 
-        while step_count > 0:
-            step_count -= 1
-            if direction == 'forward':
-                self.dir_pin.value(0)
-            elif direction == 'backward':
-                self.dir_pin.value(1)
+        if direction == CCW:
+            self.direction = CCW
+            self.dir_pin.value(CCW)
+        else:
+            self.direction = CW
+            self.dir_pin.value(CW)
 
+    def move_steps(self, steps):
+        '''
+        Function to move motor a given number of steps.
+        '''
+
+        if self.enabled is False:
+            raise ValueError("The stepper motor must be enabled to operate.")
+
+        # Positive steps rotate counter-clockwise.
+        # Negative steps rotate clockwise.
+        if steps > 0:
+            self.set_direction(CCW)
+        elif steps < 0:
+            self.set_direction(CW)
+
+        steps_to_do = abs(steps)
+        while steps_to_do > 0:
+            steps_to_do -= 1
             self.step_pin.value(0)
-            utime.sleep_us(1)
             self.step_pin.value(1)
-            utime.sleep_us(400 + delay)
-
-    def stop(self):
-        '''
-        Function to stop motor.
-        '''
-        self.step_pin.value(0)
+            utime.sleep_us(self.delay)
 
 
-if __name__ == "__main__":
-
-    # Creating stepper object
-    stepper1 = Stepper(
-        dir_pin=0,
-        step_pin=1,
-        enable_pin=2,
-        mode_pins=(3, None, None),
-        step_mode='1/2',
-        steps_per_rev=400)
-
-    delay = 1000  # delay time in ms
+if __name__ == '__main__':
 
     try:
+        stepper = Stepper(200, 0, 1, 2)
+        stepper.set_speed(200)
 
-        stepper1.enable()
+        stepper.enable()
+        rpm = stepper.get_speed()
+        print(f'SPEED: {rpm} RPM')
 
         while True:
-            stepper1.move_steps('forward', 2000, delay=0)
-            utime.sleep_ms(delay)
-            stepper1.move_steps('backward', 2000, delay=0)
-            utime.sleep_ms(delay)
+            stepper.move_steps(200)
+            utime.sleep(2)
+            
+            stepper.move_steps(-200)
+            utime.sleep(5)
 
     except KeyboardInterrupt:
-        stepper1.disable()
+        stepper.disable()
